3fb8de5f809d84e0ec26bf03245d25a4c4bd6a48
kristjanjansen <kristjan.jansen@gmail.com>
2019-06-11 14:21:08 +0300

    Push!

--- Content.js
+++ Content.js
@@ -1,5 +1,6 @@
 export const Content = content => `
 <article>
   <h2>${ content.title.rendered }</h2>
+  ${ content.content.rendered }
 </article>
 `
--- Fullwidth.js
+++ Fullwidth.js
@@ -2,7 +2,7 @@ import { Content } from "./Content.js";
 
 export const Fullwidth = content => `
 <header>
-<h1>Header</h1>
+<h1>Teretere</h1>
 </header>
 <main>
 ${content.map(Content).join('')}
